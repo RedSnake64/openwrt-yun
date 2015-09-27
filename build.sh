806ed51ca64a88001a32a86367557b2acd4ceb2e
RedSnake64 <RedSnake64Dev@t-online.de>
2015-09-27 17:34:06 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -ex
 
+#CHECK CONFIG FILE
+if [ \! `test` -f '.config' ];
+then
+	cp config.default .config
+fi
+
+#ClEANUP
 make clean
 
-#DELETE CONFIG
-rm -f .config
-
-#BUILD ONLY YUN PACKAGES
-#cp config.default .config
-
-#BUILD YUN PACKAGES AND OPENWRT PACKAGES
-cp config.yun.default .config
-
-# FEEDS
+#FEEDS
 ./scripts/feeds uninstall -a
 rm -rf feeds
 ./scripts/feeds update -a
@@ -20,13 +18,26 @@ rm -rf feeds
 #UNINSTALL BROKEN PACKAGES
 ./scripts/feeds uninstall redsocks seafile-ccnet seafile-server sstp-client tmux freeswitch umurmur-openssl umurmur-polarssl 
 
-# DELETE PACKAGES
+#DELETE PACKAGES
 rm -rf ./package/feeds/packages/avrdude
 rm -rf ./package/feeds/packages/rng-tools
 
-# LINK CUSTOM PACKAGES
+#LINK CUSTOM PACKAGES
 ln -s ../../../feeds/arduino/avrdude ./package/feeds/arduino/avrdude
 ln -s ../../../feeds/arduino/rng-tools ./package/feeds/arduino/rng-tools
+
+#DELETE CONFIG
+rm -f .config
+
+#BUILD OPTIONS
+if [ "$1" == 'YUN_PACKAGES_ONLY' ];
+then
+	#BUILD ONLY YUN PACKAGES
+	cp config.default .config
+else
+	#BUILD YUN PACKAGES AND OPENWRT PACKAGES
+	cp config.yun.default .config
+fi
 
 make oldconfig
 
