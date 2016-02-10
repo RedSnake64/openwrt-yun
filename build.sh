c40e2f8ccc298886effbc43ee1cce3ac74f50f44
RedSnake64 <RedSnake64Dev@t-online.de>
2016-02-10 18:29:47 +0100

    Update to v1.3

--- build.sh
+++ build.sh
@@ -21,10 +21,20 @@ rm -rf feeds
 #DELETE PACKAGES
 rm -rf ./package/feeds/packages/avrdude
 rm -rf ./package/feeds/packages/rng-tools
+rm -rf ./package/feeds/packages/python3
+rm -rf ./package/feeds/packages/python3-bottle
+rm -rf ./package/feeds/routing/batctl
+rm -rf ./package/feeds/routing/batman-adv
+rm -rf ./package/feeds/routing/batmand
 
 #LINK CUSTOM PACKAGES
 ln -s ../../../feeds/arduino/avrdude ./package/feeds/arduino/avrdude
 ln -s ../../../feeds/arduino/rng-tools ./package/feeds/arduino/rng-tools
+ln -s ../../../feeds/custom/python3 ./package/feeds/custom/python3
+ln -s ../../../feeds/custom/python3-bottle ./package/feeds/custom/python3-bottle
+ln -s ../../../feeds/custom/batctl ./package/feeds/custom/batctl
+ln -s ../../../feeds/custom/batman-adv ./package/feeds/custom/batman-adv
+ln -s ../../../feeds/custom/batmand ./package/feeds/custom/batmand
 
 #DELETE CONFIG
 rm -f .config
